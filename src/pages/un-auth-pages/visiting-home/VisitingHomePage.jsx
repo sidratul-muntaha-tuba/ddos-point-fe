9475a6d5d24e9d3c42110fb7dbb7b2d9f7c2e323
Sidratul Muntaha Tuba <sidratul15-13585@diu.edu.bd>
2024-05-15 23:35:49 +0600

    added lottie file for guest page

--- src/pages/un-auth-pages/visiting-home/VisitingHomePage.jsx
+++ src/pages/un-auth-pages/visiting-home/VisitingHomePage.jsx
@@ -1,6 +1,10 @@
+import Lottie from "lottie-react";
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import guestAnimation from "../../../assets/animations/guest.json";
+import { Button } from "../../../ui-components/ui/button";
+import { Label } from "../../../ui-components/ui/label";
 
 const VisitingHomePage = () => {
 	const currentUser = useSelector((state) => state.loggedInUser || null);
@@ -12,7 +16,23 @@ const VisitingHomePage = () => {
 		}
 	}, [currentUser]);
 
-	return <div>VisitingHomePage</div>;
+	return (
+		<div className="text-center">
+			<Lottie
+				className="aspect-auto h-[50vh]"
+				animationData={guestAnimation}
+				loop={true}
+			/>
+			<Label className="font-bold text-xl text-center">
+				Get a DDoS Check on your System Logs Now...
+			</Label>
+			<br />
+			<br />
+			<Link to="/login">
+				<Button className="text-xl p-4 w-40 font-extrabold">Login</Button>
+			</Link>
+		</div>
+	);
 };
 
 export default VisitingHomePage;
